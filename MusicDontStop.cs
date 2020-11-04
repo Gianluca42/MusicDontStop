d0dee4c9882e3cf2f77b1f460c980fa7a199dc7e
Gianluca42 <55921840+Gianluca42@users.noreply.github.com>
2020-11-04 17:07:27 -0300

    Add files via upload

---
+++ MusicDontStop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicDontStop : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    private static MusicDontStop instance = null;
+
+    public static MusicDontStop Instance
+    {
+        get { return instance; }
+
+    }
+    private void Awake()
+    {
+
+        if (instance != null && instance != this)
+        {
+
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
